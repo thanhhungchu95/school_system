7447dc2588f954f286498c8299c398b5edf87d71
Thanh-Hung Chu <thanhhungchu95@gmail.com>
2025-02-12 16:36:26 +0700

    Add scaffold Student module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    StudentModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
